5aa373a95aa27bddb7f72f72ed5b0da9e5e4ed8d
B-Goated <maxence.matteucci.guigui@aep95-leonarddevinci.fr>
2025-01-21 09:26:13 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,5 +2,4 @@ function getRandomInt(max) { // number between 1 & max
     return ( Math.floor(Math.random() * Math.floor(max)) )+1;
 }
 
-let img = document.getElementById("apiResponse");
 window.location.href = "./images/" + getRandomInt(6) +".png";
